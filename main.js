dc80075423206f79ae00bcf8286322285b3a834b
Baraa N. Mohsin <87875062+BaraaNazar@users.noreply.github.com>
2023-08-18 19:39:17 +0300

    Link html with main.js and create variables for each element

---
+++ main.js
@@ -0,0 +1,14 @@
+import userInfo from './userInfo.json' assert { type: 'json' };
+
+let userName = document.querySelector('.userName');
+let userJob = document.querySelector('.userJob');
+let userBio = document.querySelector('.userBio');
+let userImg = document.querySelector('.userImg');
+let projectName = document.querySelectorAll('.projectName');
+let projectDescription = document.querySelectorAll('.projectDescription');
+let projectLink = document.querySelectorAll('.projectLink');
+let projectImg = document.querySelectorAll('.projectImg');
+let blogName = document.querySelectorAll('.blogName');
+let blogDescription = document.querySelectorAll('.blogDescription');
+let blogLink = document.querySelectorAll('.blogLink');
+let blogImg = document.querySelectorAll('.blogImg');
